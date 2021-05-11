7fa24e0e34c4926b51b3311356202876b12b6ac8
Shashi Kumar <kumarshashi5294@gmail.com>
2021-05-11 18:49:42 +0530

    :tada: first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:admin_dashboard/app/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: bgColor,
+          textTheme: GoogleFonts.poppinsTextTheme(
+              Theme.of(context).textTheme.apply(bodyColor: Colors.white))),
+      home: DashboardScreen(),
+    );
+  }
+}
