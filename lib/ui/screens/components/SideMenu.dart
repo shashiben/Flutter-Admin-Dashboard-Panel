14d8e17ef4b06631fcee9368e1ec29beb4a304a2
Shashi Kumar <kumarshashi5294@gmail.com>
2021-05-12 21:57:13 +0530

    :heavy_heart_exclamation: updated logo

--- lib/ui/screens/components/SideMenu.dart
+++ lib/ui/screens/components/SideMenu.dart
@@ -1,3 +1,4 @@
+import 'package:admin_dashboard/app/constants.dart';
 import 'package:flutter/material.dart';
 
 import '../../widgets/MenuItemTile.dart';
@@ -10,51 +11,55 @@ class SideMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            DrawerHeader(child: Image.asset("assets/images/logo.png")),
-            DrawerListTile(
-              onPressed: () {},
-              image: "assets/icons/menu_dashboard.svg",
-              title: "Dashboard",
-            ),
-            DrawerListTile(
-              onPressed: () {},
-              image: "assets/icons/menu_tran.svg",
-              title: "Transactions",
-            ),
-            DrawerListTile(
-              onPressed: () {},
-              image: "assets/icons/menu_tran.svg",
-              title: "Task",
-            ),
-            DrawerListTile(
-              onPressed: () {},
-              image: "assets/icons/menu_doc.svg",
-              title: "Documents",
-            ),
-            DrawerListTile(
-              onPressed: () {},
-              image: "assets/icons/menu_store.svg",
-              title: "Store",
-            ),
-            DrawerListTile(
-              onPressed: () {},
-              image: "assets/icons/menu_notification.svg",
-              title: "Notifications",
-            ),
-            DrawerListTile(
-              onPressed: () {},
-              image: "assets/icons/menu_profile.svg",
-              title: "Profile",
-            ),
-            DrawerListTile(
-              onPressed: () {},
-              image: "assets/icons/menu_setting.svg",
-              title: "Settings",
-            ),
-          ],
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        color: secondaryColor,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              DrawerHeader(child: Image.asset("assets/images/logo.png")),
+              DrawerListTile(
+                onPressed: () {},
+                image: "assets/icons/menu_dashboard.svg",
+                title: "Dashboard",
+              ),
+              DrawerListTile(
+                onPressed: () {},
+                image: "assets/icons/menu_tran.svg",
+                title: "Transactions",
+              ),
+              DrawerListTile(
+                onPressed: () {},
+                image: "assets/icons/menu_tran.svg",
+                title: "Task",
+              ),
+              DrawerListTile(
+                onPressed: () {},
+                image: "assets/icons/menu_doc.svg",
+                title: "Documents",
+              ),
+              DrawerListTile(
+                onPressed: () {},
+                image: "assets/icons/menu_store.svg",
+                title: "Store",
+              ),
+              DrawerListTile(
+                onPressed: () {},
+                image: "assets/icons/menu_notification.svg",
+                title: "Notifications",
+              ),
+              DrawerListTile(
+                onPressed: () {},
+                image: "assets/icons/menu_profile.svg",
+                title: "Profile",
+              ),
+              DrawerListTile(
+                onPressed: () {},
+                image: "assets/icons/menu_setting.svg",
+                title: "Settings",
+              ),
+            ],
+          ),
         ),
       ),
     );
